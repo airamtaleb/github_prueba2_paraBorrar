e8590d3dabbee3edb3db48bee065333fda9c5828
txinpower <51758559+txinpower@users.noreply.github.com>
2021-03-18 19:48:42 +0100

    Initial commit

---
+++ app/src/main/java/com/politecnico/github_prueba2_paraborrar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.politecnico.github_prueba2_paraborrar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
